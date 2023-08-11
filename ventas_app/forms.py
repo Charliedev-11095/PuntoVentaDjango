22aa705aa70a4fda5de40ed20fca490327b47f5c
CharlX <Carlosroyiga@gmail.com>
2023-08-11 12:32:06 -0600

    cambio

--- ventas_app/forms.py
+++ ventas_app/forms.py
@@ -1,29 +1,27 @@
 # ventas/forms.py
-
-# ventas/forms.py
-
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
 
-class RegistroForm(UserCreationForm):
+from django import forms
+from .models import Usuario
+
+class RegistroForm(forms.ModelForm):
     email = forms.EmailField(label="Correo electrónico", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Usuario
-        fields = ['username','nombre', 'email','phone' , 'password1', 'password2', 'first_name', 'last_name','gender','birth_date','is_staff', 'es_vendedor']
+        fields = ['user_name','nombre','email', 'apellido_paterno','apellido_materno', 'gender', 'phone', 'birth_date','is_staff', 'es_vendedor']
 
     def __init__(self, *args, **kwargs):
         super(RegistroForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['user_name'].widget.attrs.update({'class': 'form-control'})
         self.fields['nombre'].widget.attrs.update({'class': 'form-control'})
-        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
-        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
-        self.fields['email'].widget.attrs.update({'class': 'form-control'})
-        self.fields['phone'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        self.fields['apellido_paterno'].widget.attrs.update({'class': 'form-control'})
+        self.fields['apellido_materno'].widget.attrs.update({'class': 'form-control'})
         self.fields['gender'].widget.attrs.update({'class':'form-control'})
+        self.fields['phone'].widget.attrs.update({'class': 'form-control'})
         self.fields['birth_date'].widget.attrs.update({'class': 'form-control'})
-        self.fields['is_staff'].widget.attrs.update({'class': ''})
         self.fields['es_vendedor'].widget.attrs.update({'class': ''})
+        self.fields['is_staff'].widget.attrs.update({'class': ''})
--- ventas_app/models.py
+++ ventas_app/models.py
@@ -5,29 +5,61 @@ from django.contrib.auth.models import User
 from django.conf import settings 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El correo electrónico debe ser proporcionado')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user( password, **extra_fields)
 
 
-class Usuario(AbstractUser):
+class Usuario(AbstractBaseUser, PermissionsMixin):
     GENDER_CHOICES = [
         ('masculino', 'Masculino'),
         ('femenino', 'Femenino'),
         ('otro', 'Otro'),
     ]
 
+    user_name=models.CharField(("Usuario"),max_length=50,unique=True)
+    email = models.EmailField(("Correo"),unique=True)
     nombre = models.CharField(max_length=30, blank=True)
-    first_name = models.CharField(('Apellido Paterno'), max_length=30, blank=True)
-    last_name = models.CharField(('Apellido Materno'), max_length=30, blank=True)
-    gender = models.CharField(('Género'), max_length=9, choices=GENDER_CHOICES, blank=True)
-    phone = models.CharField(('Teléfono'), max_length=10, blank=True)
-    email = models.EmailField(unique=True)
-    birth_date = models.DateField(('Fecha de Nacimiento'), null=True, blank=True)
-    is_active = models.BooleanField(("Activo"), default=True, help_text=("Designa si este usuario debe tratarse como activo. " "Desactive este campo en lugar de eliminar usuarios."),)
-    is_staff = models.BooleanField(("Es Administrador"), default=False, help_text=("Indica si el usuario puede ingresar a este sitio de administración."),)
-    es_vendedor = models.BooleanField(default=False)
+    apellido_paterno=models.CharField(max_length=30, blank=True)
+    apellido_materno=models.CharField(max_length=30, blank=True)
+    gender = models.CharField(("género"),max_length=9, choices=GENDER_CHOICES, blank=True)
+    phone = models.CharField(("Téléfono"),max_length=10, blank=True)
+    birth_date = models.DateField(("Fecha de Nacimiento"),null=True, blank=True)
+    is_active = models.BooleanField(("está activo"),default=True)
+    is_staff = models.BooleanField(("es trabajador"),default=False)
+    es_vendedor = models.BooleanField(("es vendedor"),default=False)
+
+    objects = UsuarioManager()
+
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.username
-
+        return self.user_name
+    
+class contrasenas (models.Model):
+    contrasena_actual = models.CharField(max_length=50,null=False)
+    contrasena_nueva = models.CharField(max_length=50,null=False)
+    repetir_contrasena = models.CharField(max_length=50,null=False)
 
 #Tabla Marcas
 class marca(models.Model):
