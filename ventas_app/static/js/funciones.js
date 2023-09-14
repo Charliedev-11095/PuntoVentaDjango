756249a6cc6eccfd361b1b3d79a65ccdeb742983
Charliedev-11095 <carlosroyiga@gmail.com>
2023-09-14 10:54:07 -0600

    ñam

--- ventas_app/static/js/funciones.js
+++ ventas_app/static/js/funciones.js
@@ -7,3 +7,24 @@ $(function () {
         yearRange: "1900:{{ 'now'|date:'Y' }}" // Ajusta el rango de años según tus necesidades
     });
 });
+
+$(document).ready(function(){
+    var table = $('#datatablesSimple').DataTable({
+       orderCellsTop: true,
+       fixedHeader: true 
+    });
+    //Creamos una fila en el head de la tabla y lo clonamos para cada columna
+    $('#datatablesSimple thead tr').clone(false).appendTo( '#datatablesSimple thead' );
+    $('#datatablesSimple thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text(); //es el nombre de la columna
+        $(this).html( '<input type="text" placeholder="Buscar Por '+title+'" />' );
+        $( 'input', this ).on( 'keyup change', function () {
+            if ( table.column(i).search() !== this.value ) {
+                table
+                    .column(i)
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );   
+});
