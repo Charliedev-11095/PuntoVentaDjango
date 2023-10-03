bf74be5616939d61ae68e2e2c73056f2c2a3b192
Charliedev-11095 <carlosroyiga@gmail.com>
2023-10-03 09:13:12 -0600

    entre 2 tierras

--- ventas_app/models.py
+++ ventas_app/models.py
@@ -70,6 +70,11 @@ class marca(models.Model):
         verbose_name_plural = 'Marcas'
     def __str__(self):
         return self.nombre_de_la_marca
+    
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+        
 
 
 class Domicilio(models.Model):
@@ -83,6 +88,9 @@ class Domicilio(models.Model):
         verbose_name = 'Domicilio'
         verbose_name_plural = 'Domicilio'
 
+    def __str__(self):
+        return self.calle + ' ' + self.no_exterior
+
 
 class DatosContacto(models.Model):
     telefono = models.CharField(max_length=10, null=True)
@@ -90,11 +98,22 @@ class DatosContacto(models.Model):
     correo_electronico = models.EmailField(max_length=250, null=True)
     url_social = models.URLField(max_length=250,null=True)
 
+    def __str__(self):
+        return self.celular + ' ' + self.correo_electronico
+
+    class Meta:
+        verbose_name = 'Dato de Contacto'
+        verbose_name_plural = 'Datos de Contacto'
+
 
 class DatosFiscales(models.Model):
     rfc = models.CharField(max_length=13, null=True)
     razon_social = models.CharField(max_length=50, null=True)
     direccion = models.ForeignKey(Domicilio, null=False, blank=False,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.rfc + ' ' + self.razon_social + ' ' 
+
     class Meta:
         verbose_name = 'Dato Fiscal'
         verbose_name_plural = 'Datos Fiscales'
@@ -104,6 +123,10 @@ class DatosPersonales(models.Model):
     nombre = models.CharField(max_length=50, null=False, blank=False)
     apellidos = models.CharField(max_length=50, null=False, blank=False)
     genero = models.CharField(max_length=1,null=True)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellidos
+
     class Meta:
         verbose_name = 'Dato Personal'
         verbose_name_plural = 'Datos Personales'
@@ -117,12 +140,23 @@ class UsuariosAcceso(models.Model):
     usuario = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     tipo = models.CharField(max_length=1)
 
+    def __str__(self):
+        return self.datos_personales.nombre + ' ' + self.datos_personales.apellidos
+
+    class Meta:
+        verbose_name = 'Usuario de Acceso'
+        verbose_name_plural = 'Usuarios de Acceso'
+
 
 #Tabla de clientes
 class Clientes(models.Model):
     datos_personales = models.ForeignKey(DatosPersonales,null=False,on_delete=models.CASCADE)
     datos_contacto = models.ForeignKey(DatosContacto,null=False,blank=False,on_delete=models.CASCADE)
     datos_fiscales = models.ForeignKey(DatosFiscales,null=True,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.datos_personales.nombre + ' ' + self.datos_personales.apellidos
+   
     class Meta:
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
@@ -132,14 +166,33 @@ class Negocio(models.Model):
     contacto = models.ForeignKey(DatosContacto, null=False, blank=False,on_delete=models.CASCADE)
     datos_fiscales = models.ForeignKey(DatosFiscales, null=False,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.datos_fiscales.razon_social
+
+    class Meta:
+        verbose_name = 'Negocio'
+        verbose_name_plural = 'Negocios'
 
 class DepartamentoProducto(models.Model):
     nombre = models.CharField(max_length=50,null=False,blank=False)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Departamento de Producto'
+        verbose_name_plural = 'Departamentos de Productos'
 
 
 class UnidadesMedidaProducto(models.Model):
     descripcion = models.CharField(max_length=50,null=False,blank=False)
 
+    def __str__(self):
+        return self.descripcion
+    
+    class Meta:
+        verbose_name = 'Unidad de Medida de Producto'
+        verbose_name_plural = 'Unidades de Medida de Productos'
 
 class Productos(models.Model):
     nombre = models.CharField(max_length=50,null=False)
@@ -149,6 +202,10 @@ class Productos(models.Model):
     deparamento_producto = models.ForeignKey(DepartamentoProducto,null=True,blank=False,on_delete=models.SET_NULL)
     precio_venta = models.DecimalField(max_digits=5,decimal_places=2,null=False)
     existencia_minima = models.DecimalField(max_digits=5,decimal_places=2,null=False)
+    
+    def __str__(self):
+        return self.nombre + ' ' + self.marca.nombre_de_la_marca + ' ' + self.deparamento_producto.nombre + ' ' + str(self.precio_venta)
+    
     class Meta:
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
@@ -158,14 +215,20 @@ class Productos(models.Model):
 class Cajas(models.Model):
     clave = models.CharField(max_length=50, null=False)
     nombre = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.clave + ' ' + self.nombre
+
     class Meta:
         verbose_name = 'Caja'
         verbose_name_plural = 'Cajas'
 OPCIONMOVIMIENTO = (
     ('a','abrir'),
-    ('c','cerrar')
-)
+    ('c','cerrar'),)
 
+class Meta:
+        verbose_name = 'Caja'
+        verbose_name_plural = 'Cajas'
 
 class MovimientosCaja(models.Model):
     caja = models.ForeignKey(Cajas,null=True,blank=False, on_delete=models.SET_NULL)
@@ -176,6 +239,12 @@ class MovimientosCaja(models.Model):
     saldo_final = models.DecimalField(max_digits=10, decimal_places=2)
     empleado = models.ForeignKey(UsuariosAcceso,null=True,blank=False,on_delete=models.SET_NULL)
     
+    def __str__(self):
+        return self.caja.nombre + ' ' + self.tipo_movimiento + ' ' + str(self.saldo_inicial) + ' ' + str(self.saldo_final)
+
+    class Meta:
+        verbose_name = 'Movimiento de Caja'
+        verbose_name_plural = 'Movimientos de Caja'
 
 #tabla ventas 
 METODOPAGOS = (
@@ -184,6 +253,8 @@ METODOPAGOS = (
     ('c','tarjeta de credito'),
     ('d','tarjeta de debito'),
 )
+
+
 
 
 class Ventas(models.Model):
@@ -197,6 +268,10 @@ class Ventas(models.Model):
     importe_pagado = models.DecimalField(max_digits=10, decimal_places=2)
     vuelto = models.DecimalField(max_digits=10, decimal_places=2)
     empleado = models.ForeignKey(UsuariosAcceso,null=True,blank=False, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.cliente.datos_personales.nombre + ' ' + self.cliente.datos_personales.apellidos + ' ' + str(self.total) + ' ' + str(self.total_descuento) + ' ' + str(self.total_impuesto) + ' ' + self.caja.nombre + ' ' + self.metodo_pago + ' ' + str(self.importe_pagado) + ' ' + str(self.vuelto) + ' ' + self.empleado.datos_personales.nombre + ' ' + self.empleado.datos_personales.apellidos
+
     class Meta:
         verbose_name = 'Venta'
         verbose_name_plural = 'Ventas'
@@ -211,6 +286,10 @@ class DetalleVentas(models.Model):
     importe = models.DecimalField(max_digits=10, decimal_places=2)
     impuesto = models.DecimalField(max_digits=5, decimal_places=2)
     venta = models.ForeignKey(Ventas,null=True,blank=False,on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.producto.nombre + ' ' + str(self.descuento) + ' ' + str(self.porcentaje_descuento) + ' ' + str(self.cantidad) + ' ' + str(self.valor_unitario) + ' ' + str(self.importe) + ' ' + str(self.impuesto) + ' ' + self.venta.cliente.datos_personales.nombre + ' ' + self.venta.cliente.datos_personales.apellidos + ' ' + str(self.venta.total) + ' ' + str(self.venta.total_descuento) + ' ' + str(self.venta.total_impuesto) + ' ' + self.venta.caja.nombre + ' ' + self.venta.metodo_pago + ' ' + str(self.venta.importe_pagado) + ' ' + str(self.venta.vuelto) + ' ' + self.venta.empleado.datos_personales.nombre + ' ' + self.venta.empleado.datos_personales.apellidos
+    
     class Meta:
         verbose_name = 'Detalle de Venta'
         verbose_name_plural = 'Detalle de Ventas'
